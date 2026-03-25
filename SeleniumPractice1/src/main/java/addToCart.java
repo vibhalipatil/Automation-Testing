fb2ca3d702ec00f444fecf2dc7eca33d2d698656
HP <HP@DESKTOP-CSDJ6CH>
2026-03-25 23:02:11 +0530

    addtocart.java

--- SeleniumPractice1/src/main/java/addToCart.java
+++ SeleniumPractice1/src/main/java/addToCart.java
@@ -24,7 +24,6 @@ public class addToCart {
 		
 	}
 	
-	
 	public static void additeams(WebDriver driver ) {
 		String[] items = {"Brocolli", "Beans","Mushroom"}; 
 		List<WebElement> products = driver.findElements(By.className("product-name"));
