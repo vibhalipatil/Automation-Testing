48f8ecdb2047119d7031dd64e31534488a8f7d0d
vibhalipatil <vibhalipatil.alibag@gmail.com>
2026-04-09 19:34:45 +0530

    Window Handling

--- SeleniumPractice1/src/main/java/windowhandling.java
+++ SeleniumPractice1/src/main/java/windowhandling.java
@@ -1,4 +1,4 @@
-import java.util.Iterator;
+import java.time.Duration;
 import java.util.Set;
 
 import org.openqa.selenium.By;
@@ -7,21 +7,25 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class windowhandling {
 
-	public static void main(String[] args) {
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://rahulshettyacademy.com/loginpagePractise/#");
-		driver.findElement(By.cssSelector(".blinkingText")).click();
-        Set<String> windows = driver.getWindowHandles(); // Get all window handles
-        Iterator<String> it = windows.iterator();   // Create Iterator
-        String parentid = it.next();// First window = Parent
-        String childid = it.next();     // Second window = Child
-        driver.switchTo().window(childid);   //tell selenium to switch to child window
-        // System.out.println(driver.findElement(By.cssSelector(".im-para.red")).getText());
-        String email= (driver.findElement(By.cssSelector(".im-para.red")).getText()).split("at")[1].trim().split(" ")[0];
-        driver.switchTo().window(parentid);
-        driver.findElement(By.xpath("//*[@id=\"username\"] ")).sendKeys(email);
-		
-	}
+    public static void main(String[] args) {
 
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+        driver.get("https://rahulshettyacademy.com/loginpagePractise/#");
+        driver.findElement(By.cssSelector(".blinkingText")).click();
+        String parentWindow = driver.getWindowHandle();
+        Set<String> windows = driver.getWindowHandles();
+        for (String window : windows) {
+            if (!window.equals(parentWindow)) {
+                driver.switchTo().window(window);
+            }
+        }
+        String text = driver.findElement(By.cssSelector(".im-para.red")).getText();
+        String email = text.split("at")[1].trim().split(" ")[0];
+        driver.switchTo().window(parentWindow);
+        driver.findElement(By.id("username")).sendKeys(email);
+//        driver.quit();
+    }
 }
